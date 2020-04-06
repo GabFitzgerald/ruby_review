3a4b2aef03c3c988c82ed08277a16d7404dc0056
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-04-06 11:27:50 +1000

    pseudocode written

---
+++ Q3.rb
@@ -0,0 +1,13 @@
+# Create a method that takes a number as input and returns "This is a prime number", if its a prime number, else, "This is not a prime number"
+
+def is_it_prime(number)
+
+
+    
+# if prime 
+# return "This is a prime number"
+
+# if not prime
+# return "This is not a prime number"
+
+end
