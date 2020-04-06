2553648dda69c0c6b198c6da2d84e2082b5f3077
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-04-06 11:25:09 +1000

    Q2 answered

---
+++ Q2.rb
@@ -0,0 +1,7 @@
+# Create a method called highest_number that takes in two numbers and returns the higher of the two
+
+def highest_number(a, b)
+    return a > b ? a : b
+end
+
+p highest_number(1, 2)
